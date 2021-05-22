832173902148cd8ed3b83e4e44c87cb8e936a119
hendrik <hendrik@sol.fritz.box>
2021-05-22 20:04:37 +0200

    add tty to backup, rename executable

--- ncbackup.sh
+++ ncbackup.sh
@@ -8,7 +8,7 @@ echo "Set Nextcloud maintenance mode: on"
 docker exec -it -u www-data $CONTAINER_APP php occ maintenance:mode --on
 
 echo "Backup MySQL database using LOCK script"
-docker-mysqlbackup $CONTAINER_DB $NC_ENV_FILE
+mysqlbackup $CONTAINER_DB $NC_ENV_FILE
 
 echo "Set Nextcloud maintenance mode: off"
 docker exec -it -u www-data $CONTAINER_APP php occ maintenance:mode --off
